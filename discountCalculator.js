b4e49e0158bb4c65c4fc31fda1dfba5c9acc5e58
sheenawangumo <sheenawangumo.contact@gmail.com>
2026-04-15 02:01:34 +0300

    fixed

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,18 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    debugger;
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+    debugger;
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
 
     return totalPrice;
 }
-
+calculateDiscountedPrice (5, 10);
 
 
 module.exports = calculateDiscountedPrice;
